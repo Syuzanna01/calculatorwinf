b386b246a8c8721f88e2f7eb00bd6c53a45ca5ab
Syuzanna Manukyan <syuzmanukyan0201@gmail.com>
2022-09-15 02:04:25 -0700

    Add project files.

---
+++ calculatorwinf/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace calculatorwinf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Box2 = new System.Windows.Forms.TextBox();
+            this.Box3 = new System.Windows.Forms.TextBox();
+            this.Box1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SpringGreen;
+            this.button1.Location = new System.Drawing.Point(201, 181);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 46);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Count";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.comboBox1.Location = new System.Drawing.Point(220, 99);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(55, 24);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // Box2
+            // 
+            this.Box2.Font = new System.Drawing.Font("Nirmala UI", 16.2F);
+            this.Box2.Location = new System.Drawing.Point(323, 84);
+            this.Box2.Multiline = true;
+            this.Box2.Name = "Box2";
+            this.Box2.Size = new System.Drawing.Size(117, 48);
+            this.Box2.TabIndex = 10;
+            // 
+            // Box3
+            // 
+            this.Box3.Font = new System.Drawing.Font("Nirmala UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Box3.Location = new System.Drawing.Point(56, 262);
+            this.Box3.Multiline = true;
+            this.Box3.Name = "Box3";
+            this.Box3.Size = new System.Drawing.Size(384, 42);
+            this.Box3.TabIndex = 11;
+            // 
+            // Box1
+            // 
+            this.Box1.Font = new System.Drawing.Font("Nirmala UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Box1.Location = new System.Drawing.Point(56, 84);
+            this.Box1.Multiline = true;
+            this.Box1.Name = "Box1";
+            this.Box1.Size = new System.Drawing.Size(116, 48);
+            this.Box1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(497, 383);
+            this.Controls.Add(this.Box1);
+            this.Controls.Add(this.Box3);
+            this.Controls.Add(this.Box2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox Box2;
+        private System.Windows.Forms.TextBox Box3;
+        private System.Windows.Forms.TextBox Box1;
+    }
+}
+
---
+++ calculatorwinf/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculatorwinf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            double a, b;
+            a = Convert.ToDouble(Box1.Text);
+            b = Convert.ToDouble(Box2.Text);
+
+            switch (comboBox1.Text)
+            {
+                case "+":
+                    Box1.Text = Convert.ToString(a + b);
+                    Box3.Text = Convert.ToString($"{a} + {b} = {a + b}");
+                    break;
+                case "-":
+                    Box1.Text = Convert.ToString(a - b);
+                    Box3.Text = Convert.ToString($"{a} - {b} = {a - b}");
+                    break;
+                case "*":
+                    Box1.Text = Convert.ToString(a * b);
+                    Box3.Text = Convert.ToString($"{a} * {b} = {a * b}");
+                    break;
+                case "/":
+                    {
+                        if (b != 0)
+                        {
+                            Box1.Text = Convert.ToString(a / b);
+                            Box3.Text = Convert.ToString($"{a} / {b} = {a / b}");
+                        }
+                        else
+                            Box3.Text = "error";
+                        break;
+                    }
+            }
+        }
+    }
+}
